415cc66f0072dd5c9bfd6b97f2d1491f82622181
Domenico Mastrangelo <domenico.mastrangelo@emma-sleep.com>
2022-01-27 13:10:48 +0100

    Created package functionality to send notifications

---
+++ pkg/notifier/notify.go
@@ -0,0 +1,62 @@
+package notifier
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type Notifier struct {
+	Url        string
+	Messages   []string
+	ErrChannel chan error
+	Timeout    int
+}
+
+func (n *Notifier) Notify() {
+	n.checkTimeout()
+
+	for _, message := range n.Messages {
+		go n.sendMessage(message)
+	}
+}
+
+func (n *Notifier) checkTimeout() {
+	if n.Timeout < 5 {
+		n.Timeout = 5
+	}
+}
+
+func (n *Notifier) sendMessage(message string) {
+	var err error
+
+	err = n.checkUrl()
+
+	if err != nil {
+		n.ErrChannel <- err
+		return
+	}
+
+	httpClient := http.Client{
+		Timeout: time.Duration(n.Timeout) * time.Second,
+	}
+
+	_, err = httpClient.Post(n.Url, "text/plain", strings.NewReader(message))
+
+	if err != nil {
+		n.ErrChannel <- err
+	}
+
+	n.ErrChannel <- nil
+}
+
+func (n *Notifier) checkUrl() error {
+	_, err := url.ParseRequestURI(n.Url)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
